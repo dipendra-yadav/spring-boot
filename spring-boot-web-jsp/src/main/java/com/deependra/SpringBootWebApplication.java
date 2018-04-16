36e7c6af844679dad0d53cae0e2ea53843dde160
deependra <dipendra.eng@gmail.com>
2018-04-16 14:26:50 +0530

    initial commit

---
+++ spring-boot-web-jsp/src/main/java/com/deependra/SpringBootWebApplication.java
@@ -0,0 +1,22 @@
+package com.deependra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
+//This SpringBootServletInitializer run a SpringApplication from a traditional WAR deployment
+@SpringBootApplication
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootWebApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+
+}
