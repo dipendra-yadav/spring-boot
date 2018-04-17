ab33f8658d6492c2c3b18d3a7b43650485b582df
deependra <dipendra.eng@gmail.com>
2018-04-17 12:10:56 +0530

    initial commit..

---
+++ spring-boot/src/main/java/com/deependra/App.java
@@ -0,0 +1,14 @@
+package com.deependra;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from App1" );
+    }
+}
---
+++ spring-boot/src/main/java/com/deependra/App2.java
@@ -0,0 +1,14 @@
+package com.deependra;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class App2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from App2!" );
+    }
+}
