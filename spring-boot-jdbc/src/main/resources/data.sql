70665ecf690deb4a702fb29b100568eb2f20d593
deependra <dipendra.eng@gmail.com>
2018-04-19 16:02:37 +0530

    spring boot-jdbc with HickariDatasource

---
+++ spring-boot-jdbc/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO customer(name,email,created_date)VALUES('deependra','111@yahoo.com', '2017-02-11');
+INSERT INTO customer(name,email,created_date)VALUES('manoj','222@yahoo.com', '2017-02-12');
+INSERT INTO customer(name,email,created_date)VALUES('shahrukh','333@yahoo.com', '2017-02-13');
